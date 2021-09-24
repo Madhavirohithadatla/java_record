25925523831c8954307f051e7c52a7006a9aeb96
Datla Madhavi Rohitha <79572509+Madhavirohithadatla@users.noreply.github.com>
2021-09-24 12:03:04 +0530

    Add files via upload

---
+++ Lab5_e.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+class Lab5_e
+{
+public static void main(String args[])
+{
+System.out.println("before sorting strings");
+ for(int i=0;i<args.length;i++)
+ System.out.println(args[i]);
+ Arrays.sort(args);
+ System.out.println("after sorting");
+ for(int i=0;i<args.length;i++)
+ System.out.println(args[i]);
+}
+}
+
+
